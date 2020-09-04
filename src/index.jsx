bb1a894b6daca8666e101fca39967bcbc05d5ee4
Trey <qwestionmark90@gmail.com>
2020-09-04 09:06:06 -0400

    Installed apollo deps.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import App from './App';
  
 ReactDOM.render(
-  <App>App</App>,
+  <ApolloProvider>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app')
 );
 
