ce9d044bb31190665fa6c930452af1f271ba8662
Trey <qwestionmark90@gmail.com>
2020-09-17 09:01:52 -0400

    Displayed Spell data in SpellCard. Installed sass pkg.

--- src/components/SpellCard/SpellCard.tsx
+++ src/components/SpellCard/SpellCard.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { Card } from "@material-ui/core";
+import { Card, CardContent, Typography } from "@material-ui/core";
 import SpellProps from "../../types/SpellProps";
+import "./SpellCard.scss";
 
-const SpellCard = (spell: SpellProps) => {
-  return <Card>{spell.name}</Card>;
+const SpellCard = ({ name, desc, range, casting_time }: SpellProps) => {
+  return (
+    <Card raised={true} className="SpellCard">
+      <CardContent>
+        <Typography variant="h6">{name}</Typography>
+        <Typography>Description: {desc[0]}</Typography>
+        <Typography>Range: {range}</Typography>
+        <Typography>Casting Time: {casting_time}</Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default SpellCard;
--- src/components/SpellResults/SpellResults.tsx
+++ src/components/SpellResults/SpellResults.tsx
@@ -4,16 +4,30 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import spellsQuery from "../../graphql/queries/spellsQueries";
 
+// MUI imports
+import { Grid } from "@material-ui/core";
+
 // Component imports
 import SpellCard from "../SpellCard/SpellCard";
 import SpellProps from "../../types/SpellProps";
 
 const SpellResults = () => {
   const { data, loading, error } = useQuery(spellsQuery);
-  if (loading) return "Loading...";
-  if (error) return `Error! ${error.message}`;
+  if (error) return <p>{`Error! ${error.message}`}</p>;
 
-  return data.spells.map((spell: SpellProps) => <SpellCard {...spell} />);
+  return (
+    <Grid container spacing={2} justify="center">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        data.spells.map((spell: SpellProps) => (
+          <Grid item>
+            <SpellCard {...spell} />
+          </Grid>
+        ))
+      )}
+    </Grid>
+  );
 };
 
 export default SpellResults;
