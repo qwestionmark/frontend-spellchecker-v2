d4118ec5c77b963174cafa73b8a9bccaa04c1d3c
Trey <qwestionmark90@gmail.com>
2020-09-17 22:09:45 -0400

    Installed react-lines-ellipsis pkg. Using in SpellCard.

--- src/components/SpellCard/SpellCard.tsx
+++ src/components/SpellCard/SpellCard.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import SpellProps from "../../types/SpellProps";
 import "./SpellCard.scss";
+import LinesEllipsis from "react-lines-ellipsis";
 
 const SpellCard = ({ name, desc, range, casting_time }: SpellProps) => {
   return (
     <Card raised={true} className="SpellCard">
       <CardContent>
-        <Typography variant="h6">{name}</Typography>
-        <Typography>Description: {desc[0]}</Typography>
-        <Typography>Range: {range}</Typography>
-        <Typography>Casting Time: {casting_time}</Typography>
+        <Typography variant="h6" align="center">
+          {name}
+        </Typography>
+        <Typography>
+          <LinesEllipsis maxLine={4} text={desc[0]} basedOn="letters" />
+        </Typography>
       </CardContent>
     </Card>
   );
