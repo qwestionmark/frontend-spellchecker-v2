2cf38e2f08333d907f867a67b96ae4bf259da7c5
Trey <qwestionmark90@gmail.com>
2020-09-18 22:13:43 -0400

    Moved spell query into custom hook.

--- src/hooks/spellHooks.ts
+++ src/hooks/spellHooks.ts
@@ -1,17 +1,26 @@
 import { useMemo } from "react";
 import SpellProps from "../types/SpellProps";
 
-function useSpellFilter({ spells, filters }) {
-  return useMemo((): SpellProps[] => {
+// Graphql imports
+import { useQuery } from "@apollo/client";
+import spellsQuery from "../graphql/queries/spellsQueries";
+
+function useSpellFilter(filters) {
+  const { data, loading, error } = useQuery(spellsQuery);
+  return useMemo((): any => {
     // If spells isn't empty and search query is long enough to be meaningful, then filter, else return all
-    let matchingSpells = [...spells];
-    if (spells.length > 0 && filters.name.length > 2) {
-      matchingSpells = spells.filter((spell) =>
-        spell.name.match(new RegExp(filters.name, "gi"))
-      );
+    let filteredSpells: SpellProps[] = [];
+    if (data && data.spells) {
+      if (filters.name.length < 3) {
+        filteredSpells = data.spells.filter((spell) =>
+          spell.name.match(new RegExp(filters.name, "gi"))
+        );
+      } else {
+        filteredSpells = data.spells;
+      }
     }
-    return matchingSpells;
-  }, [spells, filters]);
+    return { filteredSpells, loading, error };
+  }, [data, filters, loading, error]);
 }
 
 export { useSpellFilter };
--- src/pages/SpellBook.tsx
+++ src/pages/SpellBook.tsx
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 import { useSpellFilter } from "../hooks/spellHooks";
 
-// Graphql imports
-import { useQuery } from "@apollo/client";
-import spellsQuery from "../graphql/queries/spellsQueries";
-
 // Component imports
 import SpellResults from "../components/SpellResults/SpellResults";
 import SpellSearchbar from "../components/SpellSearchBar/SpellSearchbar";
 
 const SpellBook = () => {
-  const { data, loading, error } = useQuery(spellsQuery);
   const [searchFilters, setsearchFilters] = useState({
     name: "",
   });
@@ -18,10 +13,7 @@ const SpellBook = () => {
     const newFilters = { ...searchFilters, [e.target.name]: e.target.value };
     setsearchFilters(newFilters);
   };
-  const filteredSpells = useSpellFilter({
-    spells: data ? data.spells : [],
-    filters: searchFilters,
-  });
+  const { filteredSpells, loading, error } = useSpellFilter(searchFilters);
 
   return (
     <>
